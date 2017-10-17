66cded4cc294d60733ec01c951524c90f4c5306c
Vasileios Bakas <bill.bakas92@gmail.com>
2017-10-17 12:32:42 +0300

    Added SweetAlert2 as dev-dependency via webpack

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -3,6 +3,10 @@ import whatInput from 'what-input';
 
 window.$ = $;
 
+// SweetAlert2 
+
+import swal from 'sweetalert2/dist/sweetalert2.all.min.js';
+
 import Foundation from 'foundation-sites';
 // If you want to pick and choose which modules to include, comment out the above and uncomment
 // the line below
